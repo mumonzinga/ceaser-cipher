1a26c313bfc18de4167b445ca6b8c0faab64823c
Mumo Nzinga <mnzinga888@gmail.com>
2019-04-27 14:08:18 +0300

    test word encrypt

--- src/test/java/DecodeTest.java
+++ src/test/java/DecodeTest.java
@@ -1,14 +1,23 @@
 
 package test.java;
 import main.java.Ceaser.Decode;
+import org.junit.jupiter.api.Test;
+import org.testng.annotations.ITestAnnotation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DecryptTest{
-
-    public void encrypt_testLetterEncryption_String(){
+public class DecodeTest {
+    @Test
+    public void encrypt_testLetterEncryption_String() {
         Decode testDecrypt = new Decode();
-        String expectedOutput  = "H";
+        String expectedOutput = "H";
         assertEquals(expectedOutput, testDecrypt.decrypt("I", 1));
     }
+    @Test
+    public void encrypt_testWordEncryption_String(){
+        Decode testDecrypt = new Decode();
+        String expectedOutput  = "pear";
+        assertEquals(expectedOutput, testDecrypt.decrypt("qfbs", 1));
+    }
 
+}
