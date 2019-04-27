67c22b55802a759ee76e39bfd65e1506aa1f1ca4
Mumo Nzinga <mnzinga888@gmail.com>
2019-04-27 14:25:34 +0300

    test letter decode

---
+++ src/test/java/EncodeTest.java
@@ -0,0 +1,15 @@
+package test.java;
+import main.java.Ceaser.Encode;
+import org.junit.jupiter.api.Test;
+import org.testng.annotations.ITestAnnotation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class EncodeTest {
+
+    @Test
+    public void encrypt_testLetterEncryption_String() {
+        Encode testEncrypt = new Encode();
+        String expectedOutput = "I";
+        assertEquals(expectedOutput, testEncrypt.encrypt("H", 1));
+    }
+}
