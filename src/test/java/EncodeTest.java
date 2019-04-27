3a18cac04b416d98ac2172257563da580dc086f9
Mumo Nzinga <mnzinga888@gmail.com>
2019-04-27 14:27:37 +0300

    test word decode

--- src/test/java/EncodeTest.java
+++ src/test/java/EncodeTest.java
@@ -12,4 +12,10 @@ public class EncodeTest {
         String expectedOutput = "I";
         assertEquals(expectedOutput, testEncrypt.encrypt("H", 1));
     }
+    @Test
+    public void encrypt_testWordEncryption_String(){
+        Encode testEncrypt = new Encode();
+        String expectedOutput  = "qfbs";
+        assertEquals(expectedOutput, testEncrypt.encrypt("pear", 1));
+    }
 }
